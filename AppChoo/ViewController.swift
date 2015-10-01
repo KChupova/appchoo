5c347daf9d811307871df3ee747acbe26dbd307f
Kate Chupova <KChupova@gmail.com>
2015-10-01 10:45:01 +0300

    Initial Commit

---
+++ AppChoo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppChoo
+//
+//  Created by Kate Chupova on 01.10.15.
+//  Copyright (c) 2015 Kate Chupova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
